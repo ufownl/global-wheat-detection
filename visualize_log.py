c6d576e25daf15764e10487b8c327d5ef790ec97
RangerUFO <ufownl@gmail.com>
2020-05-07 23:30:41 +0800

    Implement the log visualization script

---
+++ visualize_log.py
@@ -0,0 +1,49 @@
+import re
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def visualize(lines):
+    regex = re.compile("^\[Epoch ([0-9]+)  Batch ([0-9]+)\]  batch_loss (\S+).*")
+    batch_x = []
+    batch_loss = []
+    for line in lines:
+        m = regex.match(line)
+        if m:
+            batch_x.append((int(m.group(1)), int(m.group(2))))
+            batch_loss.append(float(m.group(3)))
+    batches = max(batch_x, key=lambda x: x[1])[1]
+    batch_x = [epoch + batch / batches for epoch, batch in batch_x]
+    regex = re.compile("^\[Epoch ([0-9]+)\]  training_loss (\S+)  VOCMeanAP (\S+).*")
+    epoch_x = []
+    training_loss = []
+    validation_mAP = []
+    for line in lines:
+        m = regex.match(line)
+        if m:
+            epoch_x.append(int(m.group(1)))
+            training_loss.append(float(m.group(2)))
+            validation_mAP.append(float(m.group(3)))
+    plt.subplot(2, 1, 1)
+    plt.plot(np.array(batch_x), np.array(batch_loss), label="batch loss")
+    plt.grid(True)
+    axl = plt.subplot(2, 1, 2)
+    axl.plot(np.array(epoch_x), np.array(training_loss))
+    axl.set_ylabel("training loss")
+    axl.legend(("training loss",), loc="upper center")
+    axr = axl.twinx()
+    axr.plot(np.array(epoch_x), np.array(validation_mAP), "r", label="validation mAP")
+    axr.set_ylabel("validation mAP")
+    axr.legend(("validation mAP",), loc="upper right")
+    plt.grid(True)
+    plt.show()
+
+
+if __name__ == "__main__":
+    lines = []
+    while True:
+        try:
+            lines.append(input())
+        except EOFError:
+            break
+    visualize(lines)
