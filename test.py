a42d999b4ace1ac78bf8fb2d594a3d95be85842d
RangerUFO <ufownl@gmail.com>
2020-06-09 16:44:55 +0800

    Apply weighted boxes fusion over test time augmentation

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
+import random
 import argparse
+import numpy as np
 import mxnet as mx
 import gluoncv as gcv
 import matplotlib.pyplot as plt
 from model import load_model
 from dataset import load_image
+from ensemble_boxes import *
 
 
 def test(images, threshold, img_s, context):
@@ -12,13 +15,57 @@ def test(images, threshold, img_s, context):
     for path in images:
         print(path)
         raw = load_image(path)
-        x, _ = gcv.data.transforms.presets.yolo.transform_test(raw, short=img_s)
-        classes, scores, bboxes = model(x.as_in_context(context))
-        bboxes[0, :, 0::2] = (bboxes[0, :, 0::2] / x.shape[3]).clip(0.0, 1.0) * raw.shape[1]
-        bboxes[0, :, 1::2] = (bboxes[0, :, 1::2] / x.shape[2]).clip(0.0, 1.0) * raw.shape[0]
+        rh, rw, _ = raw.shape
+        classes_list = []
+        scores_list = []
+        bboxes_list = []
+        for _ in range(5):
+            img, flips = gcv.data.transforms.image.random_flip(raw, px=0.5, py=0.5)
+            x, _ = gcv.data.transforms.presets.yolo.transform_test(img, short=img_s)
+            _, _, xh, xw = x.shape
+            rot = random.randint(0, 3)
+            if rot > 0:
+                x = np.rot90(x.asnumpy(), k=rot, axes=(2, 3))
+            classes, scores, bboxes = model(mx.nd.array(x, ctx=context))
+            if rot > 0:
+                if rot == 1:
+                    raw_bboxes = bboxes.copy()
+                    bboxes[0, :, [0, 2]] = xh - raw_bboxes[0, :, [1, 3]]
+                    bboxes[0, :, [1, 3]] = raw_bboxes[0, :, [2, 0]]
+                elif rot == 2:
+                    bboxes[0, :, [0, 1, 2, 3]] = mx.nd.array([[xw], [xh], [xw], [xh]], ctx=context) - bboxes[0, :, [2, 3, 0, 1]]
+                elif rot == 3:
+                    raw_bboxes = bboxes.copy()
+                    bboxes[0, :, [0, 2]] = raw_bboxes[0, :, [1, 3]]
+                    bboxes[0, :, [1, 3]] = xw - raw_bboxes[0, :, [2, 0]]
+                raw_bboxes = bboxes.copy()
+                bboxes[0, :, 0] = raw_bboxes[0, :, [0, 2]].min(axis=0)
+                bboxes[0, :, 1] = raw_bboxes[0, :, [1, 3]].min(axis=0)
+                bboxes[0, :, 2] = raw_bboxes[0, :, [0, 2]].max(axis=0)
+                bboxes[0, :, 3] = raw_bboxes[0, :, [1, 3]].max(axis=0)
+            bboxes[0, :, :] = gcv.data.transforms.bbox.flip(bboxes[0, :, :], (xw, xh), flip_x=flips[0], flip_y=flips[1])
+            bboxes[0, :, 0::2] = (bboxes[0, :, 0::2] / (xw - 1)).clip(0.0, 1.0)
+            bboxes[0, :, 1::2] = (bboxes[0, :, 1::2] / (xh - 1)).clip(0.0, 1.0)
+            classes_list.append([
+                int(classes[0, i].asscalar()) for i in range(classes.shape[1])
+                    if classes[0, i].asscalar() >= 0.0
+
+            ])
+            scores_list.append([
+                scores[0, i].asscalar() for i in range(classes.shape[1])
+                    if classes[0, i].asscalar() >= 0.0
+
+            ])
+            bboxes_list.append([
+                bboxes[0, i].asnumpy().tolist() for i in range(classes.shape[1])
+                    if classes[0, i].asscalar() >= 0.0
+            ])
+        bboxes, scores, classes = weighted_boxes_fusion(bboxes_list, scores_list, classes_list)
+        bboxes[:, 0::2] *= rw - 1
+        bboxes[:, 1::2] *= rh - 1
         gcv.utils.viz.plot_bbox(raw, [
-            bboxes[0, i].asnumpy() for i in range(classes.shape[1])
-                if model.classes[int(classes[0, i].asscalar())] == "wheat" and scores[0, i].asscalar() > threshold
+            bboxes[i] for i in range(classes.shape[0])
+                if model.classes[int(classes[i])] == "wheat" and scores[i] > threshold
         ])
         plt.show()
 
