223808e3ba9b66208cfd36eb30706f0c96232ca5
RangerUFO <ufownl@gmail.com>
2020-07-17 01:12:35 +0800

    Add the copyright notice to the source files

--- dataset.py
+++ dataset.py
@@ -1,3 +1,17 @@
+#  Copyright 2020 RangerUFO
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 import os
 import cv2
 import copy
@@ -62,6 +76,7 @@ def gauss_noise(image):
     return image
 
 
+# This class was modified from YOLO3DefaultTrainTransform of GluonCV
 class YOLO3TrainTransform:
     def __init__(self, width, height, net, mean=(0.485, 0.456, 0.406),
                  std=(0.229, 0.224, 0.225), **kwargs):
